88427dd45bc114ee712313c47f0308accd4d99f6
fangbaoshun <fangbaoshun@hygon.cn>
2023-07-28 02:00:13 +0000

    crypto: Add interfaces for ecc key handling
    Change-Id: I1a4c9fc8b09be248cccc30af606a477a0d3369a3

---
+++ src/crypto/key/mod.rs
@@ -0,0 +1,8 @@
+// Copyright (C) Hygon Info Technologies Ltd.
+//
+// SPDX-License-Identifier: Apache-2.0
+
+//! Interfaces for public and private keys.
+
+pub(crate) mod ecc;
+pub(crate) mod group;
