654a5c3d76a0b8d4d7ba4c8044e2de3f66601255
fangbaoshun <fangbaoshun@hygon.cn>
2023-08-31 17:50:39 +0800

    crypto: support both 3.0.x and 1.1.1 versions of openssl
    Change-Id: Ic95d222001badacf9a187aa719853b0355d2e120

--- build.rs
+++ build.rs
@@ -3,10 +3,20 @@
 // SPDX-License-Identifier: Apache-2.0
 //
 
+use std::env;
 use std::path::Path;
 
 fn main() {
     if cfg!(feature = "hw_tests") || Path::new("/dev/csv-guest").exists() {
         println!("cargo:rustc-cfg=has_dev_csv_guest");
+    }
+    if let Ok(version) = env::var("DEP_OPENSSL_VERSION_NUMBER") {
+        let version = u64::from_str_radix(&version, 16).unwrap();
+        match version {
+            v if v < 0x1_01_01_00_0 => panic!("Unsupported openssl version:{}", version),
+            v if v < 0x3_00_00_00_0 => println!("cargo:rustc-cfg=ossl111"),
+            v if v < 0x3_01_00_00_0 => println!("cargo:rustc-cfg=ossl300"),
+            _ => panic!("Unsupported openssl version:{}", version),
+        }
     }
 }
--- src/crypto/sm.rs
+++ src/crypto/sm.rs
@@ -11,21 +11,27 @@ use std::{
     ptr,
 };
 
+#[cfg(ossl111)]
 pub const EVP_PKEY_CTRL_SET1_ID: c_int = EVP_PKEY_ALG_CTRL + 11;
 
 extern "C" {
-    pub fn EVP_PKEY_set1_EC_KEY(pkey: *mut EVP_PKEY, key: *mut EC_KEY) -> c_int;
+    #[cfg(ossl111)]
     pub fn EVP_PKEY_set_alias_type(pkey: *mut EVP_PKEY, ttype: c_int) -> c_int;
+
     pub fn EVP_MD_CTX_set_pkey_ctx(ctx: *mut EVP_MD_CTX, sctx: *mut EVP_PKEY_CTX) -> c_int;
+
+    #[cfg(ossl300)]
+    pub fn EVP_PKEY_CTX_set1_id(ctx: *mut EVP_PKEY_CTX, id: *const c_void, len: c_int) -> c_int;
 }
 
+#[cfg(ossl111)]
 #[allow(non_snake_case)]
 pub unsafe fn EVP_PKEY_CTX_set1_id(
     ctx: *mut EVP_PKEY_CTX,
     id: *const c_void,
-    id_len: size_t,
+    id_len: c_int,
 ) -> c_int {
-    EVP_PKEY_CTX_ctrl(ctx, -1, -1, EVP_PKEY_CTRL_SET1_ID, id_len as i32, id as *mut c_void)
+    EVP_PKEY_CTX_ctrl(ctx, -1, -1, EVP_PKEY_CTRL_SET1_ID, id_len, id as *mut c_void)
 }
 
 #[derive(Debug, Copy, Clone, PartialEq, Eq)]
@@ -59,17 +65,20 @@ impl SM2 {
                 bn_y,
             );
             let pkey = EVP_PKEY_new();
-            if EVP_PKEY_set1_EC_KEY(pkey, eckey) < 0 {
+            if EVP_PKEY_assign(pkey, EVP_PKEY_SM2, eckey as *mut c_void) <= 0 {
                 EVP_PKEY_free(pkey);
                 return Err(Error::new(
                     ErrorKind::InvalidData,
                     "EVP_KEY_set1_EC_KEY failed",
                 ));
             }
+
+            #[cfg(ossl111)]
             EVP_PKEY_set_alias_type(pkey, EVP_PKEY_SM2);
+
             let mctx: *mut EVP_MD_CTX = EVP_MD_CTX_new();
             let pctx = EVP_PKEY_CTX_new(pkey, ptr::null_mut());
-            EVP_PKEY_CTX_set1_id(pctx, id.as_ptr() as * mut c_void, id.len());
+            EVP_PKEY_CTX_set1_id(pctx, id.as_ptr() as * mut c_void, id.len() as c_int);
             EVP_MD_CTX_set_pkey_ctx(mctx, pctx);
             EVP_DigestVerifyInit(mctx, ptr::null_mut(), EVP_sm3(), ptr::null_mut(), pkey);
             EVP_DigestVerifyUpdate(mctx, msg.as_ptr() as * mut c_void, msg.len());
